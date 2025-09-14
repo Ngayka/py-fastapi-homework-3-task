98806c49d642debda23efb87864daaa7e4596a30
Ngayka <ngayka@gmail.com>
2025-09-14 20:34:36 +0300

    partly solution

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 from typing import cast
 
 from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy import select, delete
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -19,7 +20,75 @@ from database import (
 )
 from exceptions import BaseSecurityError
 from security.interfaces import JWTAuthManagerInterface
+from schemas.accounts import (
+    UserRegistrationResponseSchema,
+    UserRegistrationRequestSchema,
+    UserActivationRequestSchema,
+    MessageResponseSchema
+)
+
+from security.passwords import hash_password
+
 
 router = APIRouter()
 
-# Write your code here
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    status_code = 201,
+    summary="Create a new user",
+    )
+
+async def register_user(user_data: UserRegistrationRequestSchema, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(UserModel).where(UserModel.email == user_data.email))
+    exiting_user = result.scalars().first()
+    if exiting_user:
+        raise HTTPException(status_code=409, detail=(f"A user with this email {user_data.email} already exists."))
+    try:
+        new_user = UserModel(
+            email=user_data.email,
+            _hashed_password = hash_password(user_data.password),
+            group_id=1
+        )
+
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
+        return UserRegistrationResponseSchema.model_validate(new_user)
+
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(status_code=500, detail="An error occurred during user creation.")
+
+@router.post("/activate/",
+             response_model = MessageResponseSchema,
+             status_code=201,
+             summary="activate user account")
+
+async def activate_user(user_data: UserActivationRequestSchema, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(ActivationTokenModel).where(
+            ActivationTokenModel.token == user_data.token
+        ).where(ActivationTokenModel.user.has(email == user_data.email)))
+
+    token_obj = result.scalars().firts()
+
+    expires_at_utc= cast(datetime, token_obj.expires_at).replace(tzinfo=timezone.utc)
+    now_utc = cast(datetime, datetime.utcnow().replace(tzinfo=timezone.utc))
+
+    if not token_obj or expires_at_utc < now_utc:
+        raise HTTPException(status_code=400, detail="Invalid or expired activation token.")
+
+    user = token_obj.user
+    if user.is_active:
+        raise HTTPException(status_code=400, detail="User account is already active.")
+
+    user.is_active = True
+    await db.delete(token_obj)
+    await db.commit()
+    await db.refresh(user)
+    return {"message": "User account activated successfully."}
+
+
--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,57 @@
+import re
 from pydantic import BaseModel, EmailStr, field_validator
 
 from database import accounts_validators
 
 
-# Write your code here
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserRegistrationRequestSchema(UserBase):
+    password: str
+    email: EmailStr
+
+    @field_validator("password")
+    def check_password(cls, value:str) -> str:
+        return accounts_validators.validate_password_strength(value)
+
+
+class UserRegistrationResponseSchema(UserBase):
+    id: int
+    email: EmailStr
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+    pass
+
+class MessageResponseSchema(BaseModel):
+    message: str
+    pass
+
+class PasswordResetRequestSchema(BaseModel):
+    pass
+
+class PasswordResetCompleteRequestSchema(BaseModel):
+    pass
+
+class UserLoginResponseSchema(BaseModel):
+
+    pass
+
+class UserLoginRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenRefreshResponseSchema(BaseModel):
+    pass
